845a067ffdaf408579bff1b3b5b793c967ce691a
Fabio Soares <fabioars@live.com>
2019-10-13 23:55:46 -0300

    Cria componente Header

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 export default function Home() {
     return (
         <div className="Home">
-            <h1>Hello World</h1>
+            <Header title="Fase de seleção">
+                <p>Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão Gerar Meu Campeonato para prosseguir.</p>
+            </Header>
         </div>
     );
 }
