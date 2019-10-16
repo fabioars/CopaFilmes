957414f9b885c91a6e3ebdd88733f47ef3658eca
Fabio Soares <fabioars@live.com>
2019-10-15 22:37:53 -0300

    Melhora o acabamento do header

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
+import './Home.css';
 
 export default function Home() {
     return (
         <div className="Home">
-            <Header title="Fase de seleção">
-                <p>Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão Gerar Meu Campeonato para prosseguir.</p>
-            </Header>
+            <div className="container">
+                <Header title="Fase de seleção">
+                    <p>Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão Gerar Meu Campeonato para prosseguir.</p>
+                </Header>
+            </div>
         </div>
     );
 }
