1fc286cb5c0d205e58c184d206e869495d2cd635
Fabio Soares <fabioars@live.com>
2019-10-17 21:51:42 -0300

    Cria estilo do resultado

--- frontend/src/page/home/MatchResult.test.js
+++ frontend/src/page/home/MatchResult.test.js
@@ -5,20 +5,37 @@ import MatchResult from './MatchResult';
 
 describe('Movie Counter', () => {
     let container = null;
+    let match = null;
 
     beforeEach(() => {
         container = document.createElement('div');
         document.body.appendChild(container);
+
+        match = {
+            first:{
+                id: "tt4154756",
+                titulo: "Vingadores: Guerra Infinita",
+                ano: 2018,
+                nota: 8.8
+            },
+            second:{
+                id: "tt3606756",
+                titulo: "Os Incríveis 2",
+                ano: 2018,
+                nota: 8.5
+            }
+        };
     });
 
     afterEach(() => {
         unmountComponentAtNode(container);
         container.remove();
         container = null;
+        match = null;
     });
 
     it('renders correctly', () => {
-        const tree = renderer.create(<MatchResult />).toJSON();
+        const tree = renderer.create(<MatchResult match={match} />).toJSON();
 
         expect(tree).toMatchSnapshot();
     });
